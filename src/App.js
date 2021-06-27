116aff8c06a1a56c92261b3efe624b2144146910
ketaki19 <ketakimayee19@gmail.com>
2021-06-27 14:37:43 +0530

    hey

--- src/App.js
+++ src/App.js
@@ -1,29 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
-import Application from './Application'
-import App2 from './App2'
+//import logo from './logo.svg';
+//import './App.css';
+//import Application from './Application'
+//import App2 from './App2'
 
-function App() {
+//function App() {
+  //return (
+    //<div className="App">
+     //<header className="App-header">
+        //<img src={logo} className="App-logo" alt="logo" />
+        //<p>
+          //Edit <code>src/App.js</code> and save to reload.
+        //</p>
+        //<a
+          //className="App-link"
+          //href="https://reactjs.org"
+          //target="_blank"
+          //rel="noopener noreferrer"
+        //>
+          //Learn React
+        //</a>
+        //<App2/>
+        //<Application/>
+      //</header>
+    //</div>
+  //);
+//}
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch
+} from "react-router-dom";
+
+// Since routes are regular React components, they
+// may be rendered anywhere in the app, including in
+// child elements.
+//
+// This helps when it's time to code-split your app
+// into multiple bundles because code-splitting a
+// React Router app is the same as code-splitting
+// any other React app.
+
+export default function NestingExample() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <App2/>
-        <Application/>
-      </header>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+      <hr />
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function Topics() {
+  // The `path` lets us build <Route> paths that are
+  // relative to the parent route, while the `url` lets
+  // us build relative links.
+
+  return (
+    <div>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`/topics/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`/topics/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`/topics/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={"/"}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route path={`/topics/:topicId`}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  // The <Route> that rendered this component has a
+  // path of `/topics/:topicId`. The `:topicId` portion
+  // of the URL indicates a placeholder that we can
+  // get from `useParams()`.
+  let { topicId } = useParams();
+
+  return (
+    <div>
+      <h3>{topicId}</h3>
+    </div>
+  );
+}
